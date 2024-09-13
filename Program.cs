9407bd7218efdfc061e28f3fa5fe438136eebba2
Taina Alecrim <tai.estudos@gmail.com>
2024-09-12 23:20:22 -0300

    Desafio concluido de POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone NOKIA");
+Nokia nokia = new Nokia("524225555", "G50", "5555555555", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Smartphone IPHONE");
+Iphone iphone = new Iphone("5244823399", "ZP52", "66666666666", 124);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Tigrinho");
